9bfa2dfaa64d2e3526e1d104d1950fb2a13f1261
Allpax <allpax@MININT-02KJS42.m2iformation.local>
2022-09-06 10:34:51 +0200

    Ajout des tests de la gestion de degats

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
 
 
-def creation():
-    return 10
+def gestion_des_degats():
+    """
+    La fonction de gestion des dégâts prendra en compte la force de l’attaquant,
+    les PV de celui qui reçoit l’attaque ainsi que son armure.
+    Il renverra alors PV défenseur – (Force attaque – armure défenseur)
+    """
+    return None
+
+
--- test_main.py
+++ test_main.py
@@ -2,6 +2,12 @@ import main
 import pytest
 
 
-def test_main_creation():
-    a = main.creation()
-    assert a == 10
+def test_gestion_des_degats():
+    bob = ['Bob', 10, 5, 10]
+    alice = ['Alice', 20, 15, 5]
+
+    pv_defenseur = main.gestion_des_degats(bob, alice)
+
+    assert type(pv_defenseur) == int
+    assert pv_defenseur == alice[1] - (bob[2] - alice[3])
+
