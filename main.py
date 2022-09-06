2d5271da339e00d9289130a79b44549b5c44c439
Allpax <allpax@MININT-02KJS42.m2iformation.local>
2022-09-06 12:52:58 +0200

    Creation de la branche gestion_du_combat est de ses tests unitaires associé

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
 
 
-def creation():
-    return 10
+def gestion_du_combat():
+    """
+    La fonction de gestion de combat va permettre de continuer un combat
+    jusqu’à ce qu’un le joueur ou le monstre n’ait plus de point de vie,
+    et retournera le tableau de stats du joueur
+    """
+    return None
--- test_main.py
+++ test_main.py
@@ -3,5 +3,10 @@ import pytest
 
 
 def test_main_creation():
-    a = main.creation()
-    assert a == 10
+    deffs = [['a', 9, 0, 0], ['b', 10, 5, 5], ['c', 10, 0, 0]]
+    atts  = [['A', 1, 10, 0],['B', 10, 1, 1], ['C', 10, 2, 0]]
+    res = [['a',-1,0,0], ['b', 8, 5, 5], ['c', 0, 0, 0]]
+    
+    for d,a,r in zip(deffs, atts, res):
+        res = main.gestion_du_combat(d,a)
+        assert res == r
