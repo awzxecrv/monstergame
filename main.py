9db754573dea4b237e3069d912b5bb787f20cc72
Allpax <allpax@MININT-02KJS42.m2iformation.local>
2022-09-06 11:45:29 +0200

    Creation de la branche creation_du_monstre est test unitaire associe

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
 
 
-def creation():
-    return 10
+def creation_du_monstre():
+    """
+    La fonction de création d’un monstre prendra aucun paramètre 
+    mais appellera la fonction de génération de monstre en 
+    demandant à l’utilisateur le nom voulu pour le monstre.
+    La fonction renverra donc le tableau avec les statistiques du monstre.
+    """
+    return None
+
--- test_main.py
+++ test_main.py
@@ -1,7 +1,13 @@
 import main
-import pytest
 
+def test_creation_du_monstre(monkeypatch):
+    for m_name in ['Lock', 'Ness', 'Tada']:
+        monkeypatch.setattr('builtins.input', lambda _: m_name)
+        #monkeypatch.setattr('__main__.generation_du_monstre', lambda n: [n, 6, 3, 0])
 
-def test_main_creation():
-    a = main.creation()
-    assert a == 10
+        monstre = main.creation_du_monstre()
+        assert type(monstre) == list
+        assert monstre[0] == m_name
+        assert 5 <= monstre[1] <= 20
+        assert 3 <= monstre[2] <= 8
+        assert 0 <= monstre[3] <= 5
