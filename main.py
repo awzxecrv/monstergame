c1865a136c661a66f5863d751a071623e3fecc69
Allpax <allpax@MININT-02KJS42.m2iformation.local>
2022-09-06 11:06:17 +0200

    Creation de la branche et du terst pour la fonction generation du monstre

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
 
+def generation_du_monstre():
+    """
+    La fonction de génération du monstre prendra en paramètre 
+    le nom du monstre et 
+    renverra un tableau contenant l’ensemble des valeurs.
+    La valeur pour PV, Force et Armure seront des valeurs aléatoires 
+    respectivement entre 5 et 20, 3 et 8, 0 et 5
+    """
+    return None
 
-def creation():
-    return 10
--- test_main.py
+++ test_main.py
@@ -2,6 +2,13 @@ import main
 import pytest
 
 
-def test_main_creation():
-    a = main.creation()
-    assert a == 10
+def test_generation_du_monstre():
+    for _ in range(200):
+        monstre = main.generation_du_monstre('Lockness')
+    
+        assert type(monstre) == list
+        assert len(monstre) == 4
+        assert monstre[0] == 'Lockness'
+        assert 5 <= monstre[1] <= 20
+        assert 3 <= monstre[2] <= 8
+        assert 0 <= monstre[3] <= 5
