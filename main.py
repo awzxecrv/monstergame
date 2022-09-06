e904d2748bbcb435cfdadf99767f7548f64cfc1a
Allpax <allpax@MININT-02KJS42.m2iformation.local>
2022-09-06 10:42:37 +0200

    Implementation de la fonction gestion_des_degats

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 
 
-def gestion_des_degats():
+def gestion_des_degats(att, deff):
     """
     La fonction de gestion des dégâts prendra en compte la force de l’attaquant,
     les PV de celui qui reçoit l’attaque ainsi que son armure.
     Il renverra alors PV défenseur – (Force attaque – armure défenseur)
     """
-    return None
+    pv_defenseur = deff[1]
+    force_attaque = att[2]
+    armure_defenseur = deff[3]
 
-
+    pv_restant = pv_defenseur - (force_attaque - armure_defenseur)
+    return pv_restant
