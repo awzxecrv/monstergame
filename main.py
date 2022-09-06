263827c6bf424ad1f098a0f5aaa66311fc8a53e8
Allpax <allpax@MININT-02KJS42.m2iformation.local>
2022-09-06 11:58:46 +0200

    Creation de la fonction compteur, pas de test unitaire....

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
 
 
-def creation():
-    return 10
+def compteur_ennemis_tue():
+    """
+    La fonction de compteur d’ennemis tués va s’incrémenter à chaque ennemi tué 
+    et retourner cette valeur en fin de partie.
+    """
+    return None
