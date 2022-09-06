97630beca5c873e46444c5bf3809d2e3aacf596d
Emeric <allpax@MININT-02KJS42.m2iformation.local>
2022-09-06 09:52:34 +0200

    Creation de la branche creation_du_personnage et Terst unitaire associe

--- main.py
+++ main.py
@@ -1,4 +1,13 @@
 
 
-def creation():
-    return 10
+def creation_du_personnage():
+    """
+    La fonction de création d’un personnage prendra en paramètre : 
+    Pseudo, PV, Force et Armure et renverra un tableau contenant l’ensemble.
+    """
+    ####
+
+    return None
+
+
+print(help(creation_du_personnage))
--- test_main.py
+++ test_main.py
@@ -2,6 +2,12 @@ import main
 import pytest
 
 
-def test_main_creation():
-    a = main.creation()
-    assert a == 10
+def test_main_creation_du_personnage():
+    personnage = main.creation_du_personnage('Pseudo', 16, 20, 10)
+
+    assert type(personnage) == list
+    assert len(personnage) == 4
+    assert personnage[0] == 'Pseudo'
+    assert personnage[1] == 16
+    assert personnage[2] == 20
+    assert personnage[3] == 10
