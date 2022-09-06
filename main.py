732577a8232cbb19fb04e07b9422e923286c0b4d
Allpax <allpax@MININT-02KJS42.m2iformation.local>
2022-09-06 13:26:22 +0200

    Implémentation de gestion_du_combat

--- main.py
+++ main.py
@@ -1,9 +1,20 @@
 
+def gestion_des_degats(a,d):
+    return d[1] - a[2] + d[3]
 
-def gestion_du_combat():
+def gestion_du_combat(joueur, monstre):
     """
     La fonction de gestion de combat va permettre de continuer un combat
     jusqu’à ce qu’un le joueur ou le monstre n’ait plus de point de vie,
     et retournera le tableau de stats du joueur
     """
-    return None
+
+    # Tant que les deux personnages sont en vie
+    while joueur[1] > 0 and monstre[1] > 0:
+        joueur[1] = gestion_des_degats(monstre, joueur)
+        if joueur[1] <= 0:
+            continue
+
+        monstre[1] = gestion_des_degats(joueur, monstre)
+
+    return joueur
--- test_main.py
+++ test_main.py
@@ -3,10 +3,10 @@ import pytest
 
 
 def test_main_creation():
-    deffs = [['a', 9, 0, 0], ['b', 10, 5, 5], ['c', 10, 0, 0]]
-    atts  = [['A', 1, 10, 0],['B', 10, 1, 1], ['C', 10, 2, 0]]
-    res = [['a',-1,0,0], ['b', 8, 5, 5], ['c', 0, 0, 0]]
+    joueurs  = [['a', 9, 0, 0], ['b', 10, 5, 1], ['c',10, 0, 0]]
+    monstres = [['A', 1,10, 0],[ 'B', 10, 1, 1], ['C',10, 2, 0]]
+    res      = [['a',-1, 0, 0], ['b', 10, 5, 1], ['c', 0, 0, 0]]
     
-    for d,a,r in zip(deffs, atts, res):
-        res = main.gestion_du_combat(d,a)
+    for j,m,r in zip(joueurs, monstres, res):
+        res = main.gestion_du_combat(j, m)
         assert res == r
